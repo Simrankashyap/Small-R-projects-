0e0a56b5b745b56678605dd70b7144e9c86a881d
Simran kashyap <simrankashyap907@gmail.com>
2019-03-02 17:48:09 +0530

    Update iris_send_4july.r

--- iris_send_4july.r
+++ iris_send_4july.r
@@ -1,4 +1,4 @@
-#IRIS TRAINING DATA
+#IRIS TRAINING DATA IMPLEMENTATION 
 View(iris)
 row_index=sample(1:nrow(iris),0.8*nrow(iris))
 d=function(x){((x-min(x))/(max(x)-min(x)))}
@@ -14,5 +14,5 @@ predicted_table=table(pred_model,iris[-row_index,5])
 a=0.2*nrow(iris)
 diag_element=diag(predicted_table)
 accuracy_iris=(sum(diag_element)/a)*100
-accuracy_iris
+accuracy_iris  
 
